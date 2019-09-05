9ebaacd231003ff5e8f5fe82020a0a2efa182eb5
spruti <fkca.1av13.civ@gmail.com>
2019-09-05 16:19:03 +0300

    release 1.0

--- app/app.js
+++ app/app.js
@@ -1,32 +1,55 @@
-const holes = document.querySelectorAll('.hole');
-const point = document.querySelector('.points');
+const holes = document.querySelectorAll('.hole'),
+    point = document.querySelector('.points');
 
-let createbibers;
-let points = 0;
-let indexHolePrev = 0;
+let createbibers,
+    points = 0,
+    gameTime = 20000,
+    indexHolePrev = 0;
+let gameTimer = (gameTime / 1000);
 
 function createBiber() {
+
     let biber = document.createElement('img'), indexHole;
 
     do {
         indexHole = Math.floor(Math.random() * (holes.length));
     } while (indexHole == indexHolePrev);
-
+    holes[indexHole].innerHTML = "";
     indexHolePrev = indexHole;
-
+    biber.setAttribute('data-type', biber);
     biber.src = './biber_big_1_0.png';
     holes[indexHole].insertAdjacentElement('afterbegin', biber);
-    let biberImg = document.querySelector('.hole img');
-    biberImg.onclick = () => points++;
+    setTimeout(() => {
+        holes[indexHole].firstChild.classList.add('hidden');
+    }, 600);
 
-    point.innerHTML = points;
+    document.querySelector('.timer').textContent = "Осталось время: " + (gameTimer -= 0.5).toFixed(0);
 
-    console.log(indexHolePrev);
+}
+
+const hole = document.querySelector('.holes');
+hole.addEventListener('click', pointer);
+
+
+function pointer() {
+    if (event.target.dataset.type) {
+        points++;
+        point.textContent = `Бобров: ${points}`;
+        event.target.classList.add('hidden');
+    }
+}
+
+
+const $startBtn = document.querySelector('.startBtn');
+$startBtn.addEventListener('click', () => {
+    let interval = setInterval(createBiber, 500);
 
     setTimeout(() => {
-        biberImg.classList.add('hidden');
-        setTimeout(() => {
-            holes[indexHole].innerHTML = "";
-        }, 700);
-    }, 800);
-}
+        clearInterval(interval);
+        alert(`Ваш результат: ${points} бобров`)
+        points = 0
+        gameTimer = (gameTime / 1000)
+        point.textContent = `Бобров: 0`
+    }, gameTime);
+
+})
